f7dd3a1e8941b6d1835fdfd2c6b8219d0ace739a
KaranDixit1050 <124050584+KaranDixit1050@users.noreply.github.com>
2025-11-01 23:14:35 +0530

    Add files via upload

---
+++ cyclesortproblem3.java
@@ -0,0 +1,27 @@
+//https://leetcode.com/problems/find-the-duplicate-number/
+public class cyclesortproblem3 {
+    public static int findDuplicate(int[] nums) {
+        int i=0;
+        while(i<nums.length){
+            if(nums[i]!=i+1){
+                int correct=nums[i]-1;
+                if(nums[i]!=nums[correct]){
+                    int temp=nums[i];
+                    nums[i]=nums[correct];
+                    nums[correct]=temp;
+                }
+                else{
+                    return nums[i];
+                }
+            }
+            else{
+                i++;
+            }
+        }
+        return -1;
+    }
+    public static void main(String[] args){
+        int[] arr={1,3,4,2,2};
+        System.out.println(findDuplicate(arr));
+    }
+}
